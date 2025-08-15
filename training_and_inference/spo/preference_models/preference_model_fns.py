665d1b22c062e1c596adfb9840eb7f74715e3dae
Cwyxx <2274402532@qq.com>
2025-08-15 02:20:47 +0000

    2025-8-15-preference_model_fns-aigi_detector_func-jpeg_compression

--- training_and_inference/spo/preference_models/preference_model_fns.py
+++ training_and_inference/spo/preference_models/preference_model_fns.py
@@ -112,21 +112,28 @@ def pickscore_preference_model_func_builder(cfg):
 @PREFERENCE_MODEL_FUNC_BUILDERS.register_module(name="aigi_detector_preference_model_func")
 def aigi_detector_preference_model_func_builder(cfg):
     import safetensors
+    import albumentations as A
     if cfg.aigi_detector == "univfd":
         from aigi_detector_training.univfd import UnivFD
         aigi_detector = UnivFD("openai/clip-vit-large-patch14")
-        _transform = torchvision.transforms.Compose([
-            torchvision.transforms.Resize(256, interpolation=torchvision.transforms.InterpolationMode.BICUBIC),
-            torchvision.transforms.CenterCrop(224),
-            torchvision.transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        _transform = A.Compose([
+            # JPEG compress
+            A.ImageCompression(quality_range=(60, 100), p=1.0),
+            A.SmallestMaxSize(max_size=256, p=1.0),
+            A.CenterCrop(height=224, width=224, p=1.0),
+            A.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711)),
+            A.ToTensorV2(),
         ])
     elif cfg.aigi_detector == "dinov2":
         from aigi_detector_training.dinov2 import Dinov2
         aigi_detector = Dinov2("facebook/dinov2-base")
-        _transform = torchvision.transforms.Compose([
-            torchvision.transforms.Resize(256, interpolation=torchvision.transforms.InterpolationMode.BICUBIC),
-            torchvision.transforms.CenterCrop(224),
-            torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        _transform = A.Compose([
+            # JPEG compress
+            A.ImageCompression(quality_range=(60, 100), p=1.0),
+            A.SmallestMaxSize(max_size=256, p=1.0),
+            A.CenterCrop(height=224, width=224, p=1.0),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            A.ToTensorV2(),
         ])
         
     ckpt = safetensors.torch.load_file(cfg.aigi_detector_path)
@@ -134,10 +141,13 @@ def aigi_detector_preference_model_func_builder(cfg):
     aigi_detector.eval().to(cfg.device).requires_grad_(False)
     
     def preference_fn(img, extra_info):
-        img = (img / 2 + 0.5).clamp(0, 1).float()
-        img = _transform(img)
+        img = (img / 2 + 0.5).clamp(0, 1).float() # [B, C, H, W]
+        images_np = (img * 255).round().to(torch.uint8).permute(0, 2, 3, 1).cpu().numpy() # [B, C, H, W] -> [B, H, W, C]
+        
+        processed_images = [_transform(image=single_image_np)['image'] for single_image_np in images_np]
+        img_transformed = torch.stack(processed_images, dim=0).to(cfg.device)
 
-        logits = aigi_detector(img)
+        logits = aigi_detector(img_transformed)
         outputs = torch.sigmoid(logits) # 0 -> real, 1 -> fake
         scores = 1 - outputs
         
